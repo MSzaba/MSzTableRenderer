3934fade23d5835d776ad181bc2ca8a090b1dc10
MSzaba <38459524+MSzaba@users.noreply.github.com>
2021-03-28 23:56:25 +0200

    Add FIX_URL parameter to URL cell
    If the URL is not row dependent, and there is no need to parametrize that a fix URL can be used

--- MSzURLCell.php
+++ MSzURLCell.php
@@ -5,10 +5,11 @@ if (!defined("__ALLOW_INCLUDE__")) {
 require_once(__DIR__ . "/MSzCell.php");
 class MSzURLCell  implements MSzCell {
 	public const PREFIX = "PREFIX";
+	public const FIX_URL = "FIX_URL";
 
 	private $editable;
 	private $parameters;
-	private static $availableParameterList = [self::PREFIX, MSzCell::SECONDARY_PARAMETER];
+	private static $availableParameterList = [self::PREFIX, self::FIX_URL, MSzCell::SECONDARY_PARAMETER];
 
 	public function __construct($editable, $parameters = null) {
 
@@ -21,16 +22,25 @@ class MSzURLCell  implements MSzCell {
 					throw new Exception('MSzURLCell | Invalid Column parameter: ' . $parameterName);
 				}
 			}
-			if (!array_key_exists (self::PREFIX, $parameters)) {
-				throw new Exception('MSzURLCell | PREFIX parameter is not set!');
+			$prefixExists = array_key_exists (self::PREFIX, $parameters);
+			$fixURLExists = array_key_exists (self::FIX_URL, $parameters);
+			if (!$prefixExists && !$fixURLExists) {
+				throw new Exception('MSzURLCell | PREFIX or FIX_URL parameter must be set!');
+			}
+			if ($prefixExists) {
+				if (filter_var(utf8_decode  ($parameters[self::PREFIX]), FILTER_VALIDATE_URL) === FALSE) {
+					//throw new Exception('MSzURLCell | PREFIX parameter is not an URL: ' . utf8_decode   ($parameters[self::PREFIX]));
+				}
+				if (!array_key_exists (MSzCell::SECONDARY_PARAMETER, $parameters)) {
+					throw new Exception('MSzURLCell | SECONDARY_PARAMETER parameter is not set!');
+				}
+			} else {
+				if (filter_var(utf8_decode  ($parameters[self::FIX_URL]), FILTER_VALIDATE_URL) === FALSE) {
+					//throw new Exception('MSzURLCell | PREFIX parameter is not an URL: ' . utf8_decode   ($parameters[self::PREFIX]));
+				}
 			}
 			
-			if (filter_var($parameters[self::PREFIX], FILTER_VALIDATE_URL) === FALSE) {
-				throw new Exception('MSzURLCell | PREFIX parameter is not an URL!');
-			}
-			if (!array_key_exists (MSzCell::SECONDARY_PARAMETER, $parameters)) {
-				throw new Exception('MSzURLCell | SECONDARY_PARAMETER parameter is not set!');
-			}
+			
 			$this->parameters = $parameters;
 		} else {
 			throw new Exception('MSzURLCell | Parameters are not set!');
@@ -38,20 +48,30 @@ class MSzURLCell  implements MSzCell {
 	}
 
 	public function validate($value) {
-		return isset($value) && is_string($value);
+		return isset($value) && is_string($value) || is_numeric($value);
 	}
 
 	public function render($value, $secondaryValue  = NULL, $style = null) {
-		if (!isset($secondaryValue) || strlen($secondaryValue) === 0 ||  strcmp($secondaryValue, MSzCell::NULL) == 0) {
-			throw new Exception('Invalid or missing secondary render parameter!');
+		if (isset($this->parameters[self::PREFIX])) {
+			if (!isset($secondaryValue) || strlen($secondaryValue) === 0 ||  strcmp($secondaryValue, MSzCell::NULL) == 0) {
+				throw new Exception('Invalid or missing secondary render parameter!');
+			}
 		}
+		
 		if (isset($value) && strcmp($value, MSzCell::NULL) != 0 ) {
 			
 			$retVal1 = strip_tags(stripslashes(strval($value)));
-			$retVal2 = strip_tags(stripslashes(strval($secondaryValue)));
-			$styleToEnter = $style ?? "";
 			
-				return '<a href="'.$this->parameters[self::PREFIX] . $retVal2.'"' . $styleToEnter .'>' . $retVal1 . '</a>';
+			$styleToEnter = $style ?? "";
+			$link = "";
+			if (isset($this->parameters[self::PREFIX])) {
+				$retVal2 = strip_tags(stripslashes(strval($secondaryValue)));
+				$link = utf8_decode  ($this->parameters[self::PREFIX]) . $retVal2;
+			} else {
+				$link = utf8_decode  ($this->parameters[self::FIX_URL]);
+			}
+			
+				return '<a href="'. $link .'"' . $styleToEnter .'>' . $retVal1 . '</a>';
 			
 		} else {
 			return "N/A";
@@ -63,7 +83,13 @@ class MSzURLCell  implements MSzCell {
 
 	}
 	public function getSecondaryParameterId() {
-		return $this->parameters[MSzCell::SECONDARY_PARAMETER];
+		if (array_key_exists(MSzCell::SECONDARY_PARAMETER, $this->parameters)) {
+			return $this->parameters[MSzCell::SECONDARY_PARAMETER];
+		} else {
+			return null;
+		}
+		
+		
 	}
 }
 
